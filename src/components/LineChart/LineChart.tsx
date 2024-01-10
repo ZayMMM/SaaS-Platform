83bdac509f738b2ee29f1b23ba570643372552f7
Zay Maung Maung Myint <zaymaungmaungmyint@advent-soft.com>
2024-01-10 22:08:07 +0700

    remove unuse component

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -1,7 +1,7 @@
 import { Card } from "react-bootstrap";
 import CustomDatePicker from "../Date/CustomDatePicker";
 import ExportButton from "../Button/ExportButton";
-import { defaults as ChartjsDefaults, Line } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
 interface LineChartProps {
   chartTitle: string;
